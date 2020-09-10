49df1a13115da5d4934cefc1fb8eea39041f5be3
JulietK <julietgisemba000@>
2020-09-10 17:48:57 +0300

    add initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.ModelAndView;
+
+import static spark.Spark.*;
+
+public class App{
+    public static void main(String[] args) {
+
+    }
+}
